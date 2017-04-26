670e2a785e224c8bdac49b6a3e7ca98436c70a07
liwei <lwei@ulucu.com>
2017-04-26 15:36:57 +0800

    ignore

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 use EasyWeChat\Foundation\Application;
+use EasyWeChat\Message\Text;
 /**
  * Created by PhpStorm.
  * User: lwei
@@ -15,10 +16,43 @@ $options = [
     // 'aes_key' => null, // 可选
     'log' => [
         'level' => 'debug',
-        'file'  => '/wechat/easywechat.log', // XXX: 绝对路径！！！！
+        'file'  => 'G:\wechat\easywechat.log', // XXX: 绝对路径！！！！
     ],
 ];
 $app = new Application($options);
-$response = $app->server->serve();
+// 从项目实例中得到服务端应用实例。
+$server = $app->server;
+$server->setMessageHandler(function ($message) {
+    // $message->FromUserName // 用户的 openid
+    // $message->MsgType // 消息类型：event, text....
+    switch ($message->MsgType) {
+        case 'event':
+            return '收到事件消息';
+            break;
+        case 'text':
+            return new Text(['content' => '您好！overtrue。']);
+            break;
+        case 'image':
+            return '收到图片消息';
+            break;
+        case 'voice':
+            return '收到语音消息';
+            break;
+        case 'video':
+            return '收到视频消息';
+            break;
+        case 'location':
+            return '收到坐标消息';
+            break;
+        case 'link':
+            return '收到链接消息';
+            break;
+        // ... 其它消息
+        default:
+            return '收到其它消息';
+            break;
+    }
+});
+$response = $server->serve();
 // 将响应输出
 $response->send(); // Laravel 里请使用：return $response;
